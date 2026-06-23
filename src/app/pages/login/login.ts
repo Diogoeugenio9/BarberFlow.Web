61cbc620519ff9f8c16172532b3fb0b5bbfcd4c4
diogoeugenio920@gmail.com <diogoeugenio920@gmail.com>
2026-06-23 18:37:17 -0300

    refactor: criar servico de autenticacao

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,10 @@ export class Login {
   email: string = '';
   senha: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+  private http: HttpClient,
+  private authService: Auth
+) {}
 
   login() {
     const dados = {
@@ -27,7 +31,7 @@ export class Login {
     ).subscribe({
       next: (response: any) => {
 
-        localStorage.setItem('token', response.token);
+        this.authService.saveToken(response.token);
 
         console.log('Token salvo com sucesso!');
         console.log(response.token);
---
+++ src/app/services/auth.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+
+  private readonly TOKEN_KEY = 'token';
+
+  saveToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}
